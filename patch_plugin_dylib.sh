d7ed729fb0e9f948705fbff09580380b47de6133
Raphael A. Silva <rkhaotix@gmail.com>
2020-01-30 18:30:17 -0300

    Adding missing file plugins.pri

--- patch_plugin_dylib.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-install_name_tool -change @loader_path/../Frameworks/libutils.1.dylib @loader_path/../../../Frameworks/libutils.1.dylib $1
-install_name_tool -change @loader_path/../Frameworks/libparsers.1.dylib @loader_path/../../../Frameworks/libparsers.1.dylib $1
-install_name_tool -change @loader_path/../Frameworks/libpgmodeler.1.dylib @loader_path/../../../Frameworks/libpgmodeler.1.dylib $1
-install_name_tool -change @loader_path/../Frameworks/libpgmodeler_ui.1.dylib @loader_path/../../../Frameworks/libpgmodeler_ui.1.dylib $1
-install_name_tool -change @loader_path/../Frameworks/libpgconnector.1.dylib @loader_path/../../../Frameworks/libpgconnector.1.dylib $1
-install_name_tool -change @loader_path/../Frameworks/libobjrenderer.1.dylib @loader_path/../../../Frameworks/libobjrenderer.1.dylib $1
