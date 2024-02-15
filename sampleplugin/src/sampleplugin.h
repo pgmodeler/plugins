f6ce771b81c79922c115f620f6a0d4933034751a
Raphael Araújo e Silva <raphael@pgmodeler.io>
2024-02-14 22:33:12 -0300

    Minor refactor in sample plugins

--- sampleplugin/src/sampleplugin.h
+++ sampleplugin/src/sampleplugin.h
@@ -31,7 +31,7 @@ class SamplePlugin: public QObject, public PgModelerGuiPlugin {
 	private:
 		Q_OBJECT
 
-		Q_PLUGIN_METADATA(IID "io.pgmodeler.PgModelerGuiPlugin")
+		Q_PLUGIN_METADATA(IID "PgModelerGuiPlugin")
 
 		//! \brief Declares the interface which is used to implement the plugin
 		Q_INTERFACES(PgModelerGuiPlugin)
